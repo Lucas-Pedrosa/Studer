ee5a1c1068b9285b7bbfe61e1d9f19e2b062c744
Giovanni Zanotti <51122134+GiovanniZanotti@users.noreply.github.com>
2021-10-09 16:42:43 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Studer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
