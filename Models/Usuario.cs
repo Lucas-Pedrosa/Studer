00472837d4306e320d7ad3efbc915e246a9cf754
Lucas Pedrosa <lucas.dias.pedrosa@gmail.com>
2021-11-27 16:43:21 -0300

    Adicionado tipo de usuário logado

---
+++ Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studer.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string tipo { get; set; }
+    }
+}
