fd4f9596d5c3e5719b7b4294e5ffe235b97046a3
Lucas Pedrosa <lucas.dias.pedrosa@gmail.com>
2021-12-01 20:08:26 -0300

    Adicionado Login Strategy

---
+++ Controllers/Strategy/LoginStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Studer.Database;
+using Studer.Models;
+using Studer.Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studer.Controllers
+{
+    public abstract class LoginStrategy
+    {
+        internal Manager manager;
+
+        public LoginStrategy(MySqlDatabase mySqlDatabase)
+        {
+            manager = new Manager(mySqlDatabase);
+        }
+
+        public abstract Usuario Login(string email, string senha);
+    }
+}
---
+++ Controllers/Strategy/LoginStrategyEstudante.cs
@@ -0,0 +1,23 @@
+﻿using Studer.Database;
+using Studer.Models;
+using Studer.Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studer.Controllers.Strategy
+{
+    public class LoginStrategyEstudante : LoginStrategy
+    {
+
+        public LoginStrategyEstudante(MySqlDatabase mySqlDatabase) : base(mySqlDatabase)
+        {
+        }
+
+        public override Usuario Login(string email, string senha)
+        {
+            return manager.GetEstudanteDAO().login(email, senha);
+        }
+    }
+}
